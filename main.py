22d0a70c613d5d23ad4a3702aa20134f30786414
dinozavrik <dinozavrik@Dinozavrik.home.my>
2021-08-08 11:10:06 +0300

    3

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 #without context managers or 'with'
 def editf(fname,text):
     assert type(fname) is str
+    openf=open(fname,'wr')
+    try:
+        openf.write(str(text))
+        openf.read()
+    finally:
+        openf.close()
+
+#with threads and threading lock
+from threading import Lock
+
+def tl_editf(fname,text):
+    lfile=Lock()
+    lfile.acquire()  #close thread
+    try:
+        lfile.write(str(text))
+        lfile.read()
+    finally:
+        lfile.release()
+
+
+
 
